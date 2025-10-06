fe002f99d975765a1de96e404f2f53ab34d48792
ELIAS2401 <tuccieliasluis@gmail.com>
2025-10-06 12:43:44 -0300

    update 2

--- productos.js
+++ productos.js
@@ -3,92 +3,92 @@ const productos =
     [
         {
             "nombre": "Budín de Vainilla",
-            "precio": 3500,
+            "precio": 8000,
             "descripcion": "Budín casero esponjoso con aroma a vainilla.",
             "imagen": "img/imgProductos/budinVainilla.jpg",
             "mensaje": "Hola, quiero pedir el Budín de Vainilla 🍮"
         },
         {
             "nombre": "Budín de Naranja",
-            "precio": 3600,
+            "precio": 9000,
             "descripcion": "Budín suave con jugo natural de naranja.",
             "imagen": "img/imgProductos/budinNaranja.jpg",
             "mensaje": "Hola, quiero pedir el Budín de Naranja 🍊"
         },
         {
             "nombre": "Budín de Limón",
-            "precio": 3500,
+            "precio": 9000,
             "descripcion": "Budín casero con glaseado de limón.",
-            "imagen": "img/imgProductos/budinVainilla.jpg",
+            "imagen": "img/imgProductos/budinLimon.jpg",
             "mensaje": "Hola, quiero pedir el Budín de Limón 🍋"
         },
         {
             "nombre": "Budín de Mandarina",
-            "precio": 3600,
+            "precio": 9000,
             "descripcion": "Budín húmedo y aromático con mandarina fresca.",
             "imagen": "img/imgProductos/budinMandarina.jpg",
             "mensaje": "Hola, quiero pedir el Budín de Mandarina 🍊"
         },
         {
             "nombre": "Budín de Chips de Chocolate",
-            "precio": 3800,
+            "precio": 9000,
             "descripcion": "Budín esponjoso con chips de chocolate.",
             "imagen": "img/imgProductos/budinChip.jpg",
             "mensaje": "Hola, quiero pedir el Budín de Chips de Chocolate 🍫"
         },
         {
             "nombre": "Budín de Marmolado",
-            "precio": 3700,
+            "precio": 9000,
             "descripcion": "Clásico budín marmolado con vainilla y chocolate.",
             "imagen": "img/imgProductos/budinMarmolado.jpg",
-             "mensaje": "Hola, quiero pedir el Budín Marmolado 🍫🍦"
+            "mensaje": "Hola, quiero pedir el Budín Marmolado 🍫🍦"
         },
         {
             "nombre": "Budín de Banana Split",
-            "precio": 3900,
+            "precio": 9000,
             "descripcion": "Budín de banana con chips de chocolate y un toque especial.",
             "imagen": "img/imgProductos/budinBanana.jpg",
-             "mensaje": "Hola, quiero pedir el Budín de Banana Split 🍌🍫"
+            "mensaje": "Hola, quiero pedir el Budín de Banana Split 🍌🍫"
         },
         {
             "nombre": "Alfajores Marplatenses",
-            "precio": 2500,
-            "descripcion": "Alfajores rellenos con dulce de leche y baño de chocolate.",
+            "precio": 9000,
+            "descripcion": "Alfajores rellenos con dulce de leche y baño de chocolate. Vienen 9 por bandeja.",
             "imagen": "img/imgProductos/marplatense.jpg",
-             "mensaje": "Hola, me interesan los Alfajores Marplatenses 🍫"
+            "mensaje": "Hola, me interesan los Alfajores Marplatenses 🍫"
         },
         {
             "nombre": "Alfajores Pepitos",
-            "precio": 2400,
-            "descripcion": "Alfajores rellenos cubiertos con chips de chocolate.",
-            "imagen": "img/imgProductos/budinVainilla.jpg",
-             "mensaje": "Hola, me interesan los Alfajores Pepitos 🍪"
+            "precio": 9000,
+            "descripcion": "Alfajores rellenos cubiertos con chips de chocolate. Vienen 9 por bandeja.",
+            "imagen": "img/imgProductos/pepito.jpg",
+            "mensaje": "Hola, me interesan los Alfajores Pepitos 🍪"
         },
         {
             "nombre": "Alfajores de Maicena",
-            "precio": 2500,
-            "descripcion": "Clásicos alfajores con mucho dulce de leche y coco.",
-            "imagen": "img/imgProductos/budinVainilla.jpg",
-             "mensaje": "Hola, me interesan los Alfajores de Maicena 🥥"
+            "precio": 9000,
+            "descripcion": "Clásicos alfajores con mucho dulce de leche y coco. Vienen 9 por bandeja.",
+            "imagen": "img/imgProductos/maicena.jpg",
+            "mensaje": "Hola, me interesan los Alfajores de Maicena 🥥"
         },
         {
             "nombre": "Alfajores con Maní/Nueces",
-            "precio": 2600,
-            "descripcion": "Alfajores caseros con relleno y crocante de frutos secos.",
-            "imagen": "img/imgProductos/budinVainilla.jpg",
-             "mensaje": "Hola, me interesan los Alfajores con Maní o Nueces 🥜🌰"
+            "precio": 9000,
+            "descripcion": "Alfajores caseros con relleno y crocante de frutos secos. Vienen 9 por bandeja.",
+            "imagen": "img/imgProductos/mani.jpg",
+            "mensaje": "Hola, me interesan los Alfajores con Maní o Nueces 🥜🌰"
         },
         {
             "nombre": "Alfajores con Chocolate Blanco",
-            "precio": 2700,
-            "descripcion": "Alfajores rellenos bañados en chocolate blanco.",
-            "imagen": "img/imgProductos/budinVainilla.jpg",
-             "mensaje": "Hola, me interesan los Alfajores con Chocolate Blanco 🤍🍫"
+            "precio": 9000,
+            "descripcion": "Alfajores rellenos bañados en chocolate blanco. Vienen 9 por bandeja.",
+            "imagen": "img/imgProductos/chocolateBlanco.jpg",
+            "mensaje": "Hola, me interesan los Alfajores con Chocolate Blanco 🤍🍫"
         }
     ];
 
 // Número de WhatsApp
-const numeroWpp = "5491134567890";
+const numeroWpp = "5491169390101";
 
 // Renderizar productos
 const contenedor = document.getElementById("productos");
